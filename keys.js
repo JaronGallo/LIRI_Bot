96bd4c9393b62c9a6a8df3dd2dfeeec2947750cf
JaronGallo <jarongallo@yahoo.com>
2018-01-26 14:53:07 -0700

    Update keys.js

--- keys.js
+++ keys.js
@@ -2,9 +2,9 @@
 
 var twitterKeys = {
   consumer_key: '38qG2x0m5tt1QlJQ6jAUq7pSl',
-  consumer_secret: '9Kzak96QMeZkiT50XZT6fEhuGFLlmwJ33SkTjrOhodX2X5oYd7',
+  consumer_secret: 'Secret Here',
   access_token_key: '911049000987508736-9Gwd9LKVyGMgFScBA7v9RmFGlsDOBrE',
-  access_token_secret: 'Oa0xowEsH3I2r3WihvSBN89ucMLmk4VcKAMq6nPZQfRtM',
+  access_token_secret: 'Secret Here',
 };
 
 module.exports = twitterKeys;
